e2b491fea6cc42defdaf5fbf6e88f0ae682f1c45
Alessandro <alessandroamedei18@gmail.com>
2020-10-11 13:03:08 +0200

    added db config

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://admin:CKImih94798@node20776-env-2302009.it1.eur.aruba.jenv-aruba.cloud/sondip",
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  (err, db) => {
+    if (!err) {
+      console.log("connected!");
+    } else console.log("error");
+  }
+);
