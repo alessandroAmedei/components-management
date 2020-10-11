884502e3c7457f9aaeac87c4f5962c022978d457
Alessandro <alessandroamedei18@gmail.com>
2020-10-11 15:11:27 +0200

    syntaxfix..

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/api/objects", (req, res) => {
-  object.find((err, documents) => {
+  objectModel.find((err, documents) => {
     if (err) res.status(500).json(err);
     res.json(documents);
   });
@@ -26,6 +26,25 @@ app.post("/api/objects", (req, res) => {
   obj.save((err, document) => {
     if (err) res.status(500).json(err);
     res.json(document);
+  });
+});
+
+app.delete("/api/objects", (req, res) => {
+  let id = req.body.id;
+  objectModel.deleteOne({ _id: id }, (err) => {
+    if (err) res.status(500).json(err);
+    res.json({
+      deleted: "ok",
+    });
+  });
+});
+
+app.delete("/api/objects/reset", (req, res) => {
+  objectModel.deleteMany({}, (err) => {
+    if (err) res.status(500).json(err);
+    res.json({
+      deleted: "ok",
+    });
   });
 });
 
